ecbf1c2ba28aaeca20f0399755ca648da84a8dc2
Erion1993 <81769080+Erion1993@users.noreply.github.com>
2021-04-29 19:55:24 +0200

    admin produkt

--- produkt/admin.py
+++ produkt/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Produkt
 
-# Register your models here.
+
+class ProduktAdmin(admin.ModelAdmin):
+    list_display = ('id', 'serial', 'vit_prodhimi_f', 'vit_prodhimi_m')
+
+
+admin.site.register(Produkt, ProduktAdmin)
