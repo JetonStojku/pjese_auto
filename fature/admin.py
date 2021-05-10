0af18e9b81d72cba557a50d482c894406629afa4
Jeton Stojku <jetonstojku@hotmail.com>
2021-05-10 22:50:42 +0200

    Sistemime

--- fature/admin.py
+++ fature/admin.py
@@ -2,17 +2,23 @@ from django.contrib import admin
 from .models import Koke_Fature, Rrjesht_Fature
 
 
+class FatureLine(admin.TabularInline):
+    model = Rrjesht_Fature
+    extra = 1
+
+
 class Koke_FatureAdmin(admin.ModelAdmin):
+    inlines = [FatureLine]
     list_display = ('id', 'date', 'shites', 'klient')
 
 
 admin.site.register(Koke_Fature, Koke_FatureAdmin)
 
 
-
-class Rrjesht_FatureAdmin(admin.ModelAdmin):
-    list_display = ('id', 'fature_id', 'produkt_id', 'sasia', 'cmimi', 'zbritje', 'tvsh')
-
-
-admin.site.register(Rrjesht_Fature, Rrjesht_FatureAdmin)
+#
+# class Rrjesht_FatureAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'fature_id', 'produkt_id', 'sasia', 'cmimi', 'zbritje', 'tvsh')
+#
+#
+# admin.site.register(Rrjesht_Fature, Rrjesht_FatureAdmin)
 
--- produkt/views.py
+++ produkt/views.py
@@ -7,9 +7,16 @@ def index(request):
 
 
 def produkt(request):
-    produkts = Produkt.objects.order_by('-serial')
-
+    marka_emer = request.GET.get('marka')
+    if marka_emer:
+        produkts = Produkt.objects.filter(marka__emer__exact=marka_emer)
+    else:
+        produkts = Produkt.objects.all()
+    # produkts = Produkt.objects.order_by('-serial')
+    markat = Marka.objects.order_by('-emer')
     context = {
         'produkts': produkts,
+        'markat': markat,
+        'marka_emer': marka_emer
     }
     return render(request, 'produkt/produkt.html', context)
